18ccc6c92af8cae0502055e27227ee0c5b79df4f
Trần Quang Sĩ <tranquangsidev@gmail.com>
2023-05-15 08:54:12 +0700

    buoi 1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import DemoClass from "./DemoComponent/DemoClass";
+import DemoFunction from "./DemoComponent/DemoFunction";
+import DemoHeader from "./DemoComponent/DemoHeader";
+import Ex_Layout from "./Ex_Layout/Ex_Layout";
+import DataBinding from "./DataBinding/DataBinding";
+import EventHandling from "./EventHandling/EventHandling";
+import DemoState from "./DemoState/DemoState";
+import Ex_State_Car from "./Ex_State_Car/Ex_State_Car";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      {/* <DemoClass /> */}
+      {/* <DemoFunction></DemoFunction> */}
+      {/* <DemoHeader /> */}
+      {/* <Ex_Layout /> */}
+      {/* <DataBinding /> */}
+      {/* <EventHandling /> */}
+      {/* <DemoState /> */}
+      <Ex_State_Car />
     </div>
   );
 }
